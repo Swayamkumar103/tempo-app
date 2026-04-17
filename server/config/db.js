70346f83656da2a29d2453b3b9c8c9b2dcc4f760
Swayam kumar <swayamkumar103@gmail.com>
2026-04-17 23:19:06 +0530

    Updated dashboard and db config

--- server/config/db.js
+++ server/config/db.js
@@ -1,6 +1,6 @@
 // config/db.js
 // This file handles the MongoDB connection using Mongoose
-
+// mongodb+srv://swayamkumar103_db_user:swayam1234@cluster0.q30dbna.mongodb.net/productivity_app?appName=Cluster0
 import mongoose from 'mongoose';
 
 const connectDB = async () => {
@@ -9,7 +9,7 @@ const connectDB = async () => {
     // For local: 'mongodb://localhost:27017/productivity_app'
     // For Atlas: 'mongodb+srv://<user>:<pass>@cluster.mongodb.net/productivity_app'
     const conn = await mongoose.connect(
-      process.env.MONGO_URI || 'mongodb://localhost:27017/productivity_app'
+      process.env.MONGO_URI || 'mongodb+srv://swayamkumar103_db_user:swayam1234@cluster0.q30dbna.mongodb.net/productivity_app?appName=Cluster0'
     );
 
     console.log(`✅ MongoDB Connected: ${conn.connection.host}`);
